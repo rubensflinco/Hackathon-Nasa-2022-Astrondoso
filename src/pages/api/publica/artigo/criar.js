65395410dfc571cb694c66245b209604e6dbafe6
Gabriel Cardoso <gabriel.cmartins11@outlook.com>
2022-10-01 15:21:21 -0300

    Adiciona crud artigo

---
+++ src/pages/api/publica/artigo/criar.js
@@ -0,0 +1,53 @@
+import NextCors from 'nextjs-cors';
+import apiResponse from "../../../../functions/apiResponse";
+import criarSlug from "../../../../functions/criarSlug";
+import databaseConnect from "../../../../functions/databaseConnect";
+import Artigo from '../../../../models/artigo';
+import replaceAll from '../../../../functions/replaceAll';
+
+export default async function apiPublicaArtigoCriar(req, res) {
+let method = 'POST'
+
+  if (res !== null) {
+    await NextCors(req, res, {
+      methods: ['HEAD', 'OPTIONS', method],
+      origin: '*',
+      optionsSuccessStatus: 200,
+    });
+  }
+
+
+
+  if (req?.method === method) {
+    try {
+      let body = req.body;
+      let dados = body?.dados;
+      let condicoes = body?.condicoes;
+      await databaseConnect();
+
+
+
+
+      if (!dados.slug) {
+        dados.slug = criarSlug(dados.titulo);
+      }
+
+
+      let resBancoDeDados = await Artigo.create(dados);
+      return apiResponse(res, 200, "OK", "Dados criados e listados na resposta.", resBancoDeDados);
+
+
+
+    } catch (error) {
+      if (String(error).includes(`ValidationError:`)) {
+        return apiResponse(res, 400, "ERRO", replaceAll((String(error).replace("ValidationError: ", "")), ":", ""), String(error));
+      }
+
+
+      console.error(error);
+      return apiResponse(res, 500, "ERRO", "Tivemos um problema tente novamente mais tarde, caso persistir contate nossa equipe de atendimento via email.", String(error));
+    }
+  } else {
+    return apiResponse(res, 404, "ERRO", "404 endpoint não existe.", "");
+  }
+}
