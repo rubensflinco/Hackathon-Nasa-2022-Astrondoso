d0460f51e473fe7e0cac617790c23f5d53b8cecd
Rubens Flinco <contato@rubensflinco.com.br>
2022-10-22 01:28:45 -0300

    manifets json import

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,8 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta name="description" content="Astrondoso" />
+        <meta name="description" content="Neste aplicativo amigável aprenda sobre o telescópio James Webb e astronomia, e pratique seus conhecimentos com um mini-jogo de quiz" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
