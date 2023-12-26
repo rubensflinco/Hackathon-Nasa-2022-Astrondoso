2c9670d245669ea3c98ee469d7ecd7665cfc1e34
Rubens Flinco <contato@rubensflinco.com.br>
2023-12-26 02:19:01 -0300

    ajuste google tag manager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import cssGlobal from '../styles/global.module.css'
 import Router from 'next/router'
-import Head from 'next/head';
 import Script from 'next/script';
 
 
@@ -36,17 +35,15 @@ export default function App({ Component, pageProps }) {
 
 
   return (<>
-    <Head>
-      <Script id="google-tag-manager">
-      {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WQPVCXV8');
-      `}
-      </Script>
-    </Head>
+    <Script id="google-tag-manager">
+    {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WQPVCXV8');
+    `}
+    </Script>
     <div className={cssGlobal.fundoEscuro}>
       <div className={cssGlobal.espacamentoMenuCima}></div>
       <Component {...pageProps} carregando={carregando} cssGlobal={cssGlobal} />
